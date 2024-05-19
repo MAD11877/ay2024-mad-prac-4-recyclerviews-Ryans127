dea5879d306829a3ed57d83252f46db2bdf7694b
RyanSim27 <s10241631@connect.np.edu.sg>
2024-05-19 16:20:52 +0800

    fixing user

--- app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
@@ -1,11 +1,14 @@
 package sg.edu.np.mad.madpractical4;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
+import java.util.Random;
 
 public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
     private List<User> userList;
